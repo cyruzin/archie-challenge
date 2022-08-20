2d3183c176c370c35586b523d0fe6cf625ab2fdf
Cyro Dubeux <xorycx@gmail.com>
2022-08-20 12:23:41 -0300

    bug fix

--- src/config/database.ts
+++ src/config/database.ts
@@ -4,7 +4,7 @@ export const postgres = new Pool({
   host: process.env.POSTGRES_HOST,
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DATABASE,
+  database: process.env.POSTGRES_DB,
   port: Number(process.env.POSTGRES_PORT),
   max: Number(process.env.POSTGRES_MAX),
   idleTimeoutMillis: Number(process.env.POSTGRES_IDLE_TIMEOUT),
